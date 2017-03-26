83e7defb2e06fde2173a147432c1ef89f8a664e2
Tiago Barbosa <tfreirebar@gmail.com>
2017-03-26 02:18:16 -0300

    Adicionando preferencias

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -40,6 +40,7 @@ public class DetailActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         if(id == R.id.action_settings){
+            startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
